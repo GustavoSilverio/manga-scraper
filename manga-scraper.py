32225345ca4356896954c12307b118cd9bb7a35c
GustavoSilverio <gustavosk290905@gmail.com>
2024-05-05 21:09:34 -0300

    Atualização scritp

--- manga-scraper.py
+++ manga-scraper.py
@@ -4,10 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
-#Funciona somente com mangas do https://mangaschan.net Exemplo: https://mangaschan.net/04092020/quanqiu-jinru-da-hongshui-shidai-evolution-in-the-flood-capitulo-1/
-print("Funciona somente com mangas do https://mangaschan.net")
-print("Exemplo: https://mangaschan.net/04092020/quanqiu-jinru-da-hongshui-shidai-evolution-in-the-flood-capitulo-1/")
-url_first_chapter = input("Url do PRIMEIRO capítulo do manga:").strip()
+# Works just with mangas from https://mangaschan.net. Example:
+# https://mangaschan.net/04092020/quanqiu-jinru-da-hongshui-shidai-evolution-in-the-flood-capitulo-1/
+print("Works just with mangas from https://mangaschan.net")
+print("Example: https://mangaschan.net/04092020/quanqiu-jinru-da-hongshui-shidai-evolution-in-the-flood-capitulo-1/")
+url_first_chapter = input("Url of the FIRST chapter of the manga:").strip()
 
 chapters_list = []
 pages_list = []
@@ -16,7 +17,7 @@ driver = webdriver.Firefox()
 driver.get(url_first_chapter)
 
 
-# Exclui todos os iframes
+# Delete all the iframes
 def delete_iframes():
     driver.execute_script("""
     var iframes = document.getElementsByTagName('iframe');
@@ -34,11 +35,11 @@ select_chapter_element = Select(driver.find_element(by=By.ID, value="chapter"))
 select_page_element = Select(driver.find_element(by=By.ID, value="select-paged"))
 
 if not select_chapter_element:
-    print("Não achou select de capítulos!")
+    print("Don't find the chapter's select!")
     exit()
 
 if not select_page_element:
-    print("Não achou select de páginas!")
+    print("Don't find the page's select!")
     exit()
 
 select_chapter_options = reversed(select_chapter_element.options)
@@ -46,26 +47,30 @@ select_pages_options = select_page_element.options
 
 index_page = 1
 index_chapter = 1
-lista_capitulos = []
+list_chapters_text = []
 
 for cap in select_chapter_options:
     if cap.text == "Selecionar Capítulo":
         continue
-    lista_capitulos.append(cap.text)
+    list_chapters_text.append(cap.text)
 
-for cap in lista_capitulos:
+for cap in list_chapters_text:
     select_chapter_element = Select(driver.find_element(by=By.ID, value="chapter"))
+
     select_chapter_element.select_by_visible_text(cap)
+
     select_chapter_element = Select(driver.find_element(by=By.ID, value="chapter"))
+
     select_page_element = Select(driver.find_element(by=By.ID, value="select-paged"))
+
     title = driver.title.replace(" – Mangás Chan", "")
 
     if not select_chapter_element:
-        print("Não achou select de capítulos!")
+        print("Don't find the chapter's select!")
         exit()
 
     if not select_page_element:
-        print("Não achou select de páginas!")
+        print("Don't find the page's select!")
         exit()
 
     select_pages_options = select_page_element.options
@@ -79,9 +84,9 @@ for cap in lista_capitulos:
 
         url_img = driver.find_element(by=By.CLASS_NAME, value="ts-main-image").get_attribute("src")
         if not url_img:
-            print(f"Imagem não localizada, página: {page.text}")
+            print(f"Image does not exist!, page: {page.text}")
 
-        pages_list.append({"Página": index_page, "UrlCapitulo": url_img})
+        pages_list.append({"Page": index_page, "ChapterUrl": url_img})
 
         index_page += 1
 
